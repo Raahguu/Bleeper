c92fc3dd1b4f07804199426c433a49cb2dcab9d7
Raahguu <raahguu@outlook.com>
2025-06-23 13:19:50 +0800

    Now plays beep

--- Bleeper.py
+++ Bleeper.py
@@ -1,8 +1,9 @@
 import keyboard, winsound
 
+print("listening...")
 while True:
     key = keyboard.read_key()
-    if key == "space":
-        print("e")
+    if key == "space": 
+        winsound.Beep(10000, 1)
     elif key == "delete":
-        print("d")
+        break
