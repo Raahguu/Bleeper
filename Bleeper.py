a4d91af2acb606392d89cfb91668f8eb8f5435ec
Raahguu <raahguu@outlook.com>
2025-06-24 13:36:19 +0800

    Now uses a custom bleep sound, and without threading

--- Bleeper.py
+++ Bleeper.py
@@ -1,9 +1,21 @@
 import keyboard, winsound
 
+bleep_thread = None
+bleeping = False
+bleep_key = "caps lock"
+bleep_sound = "bleep.wav"
+
+def on_down(event):
+    global bleeping, bleep_thread
+    if event.name == bleep_key and not bleeping:
+        bleeping = True
+        winsound.PlaySound("bleep.wav", winsound.SND_FILENAME | winsound.SND_ASYNC | winsound.SND_LOOP)
+        print("ON")
+    elif event.name == bleep_key and bleeping:
+        bleeping = False
+        winsound.PlaySound(None, winsound.SND_PURGE)
+        print("OFF")
+
 print("listening...")
-while True:
-    key = keyboard.read_key()
-    if key == "space": 
-        winsound.Beep(10000, 1)
-    elif key == "delete":
-        break
+keyboard.on_press(on_down)
+keyboard.wait()
