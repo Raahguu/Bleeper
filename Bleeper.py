5f5380ecc1277593e50c4111ed7e709fb4a14feb
Raahguu <raahguu@outlook.com>
2025-06-24 13:43:26 +0800

    Removed deprecated bleep_thread variable

--- Bleeper.py
+++ Bleeper.py
@@ -1,12 +1,11 @@
 import keyboard, winsound
 
-bleep_thread = None
 bleeping = False
 bleep_key = "caps lock"
 bleep_sound = "bleep.wav"
 
 def on_down(event):
-    global bleeping, bleep_thread
+    global bleeping
     if event.name == bleep_key and not bleeping:
         bleeping = True
         winsound.PlaySound("bleep.wav", winsound.SND_FILENAME | winsound.SND_ASYNC | winsound.SND_LOOP)
